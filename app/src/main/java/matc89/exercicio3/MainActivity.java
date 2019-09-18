e604b0e93d4711377c4d3ab985d438595a6eab1c
marivaldo junior <marivaldojunior235@gmail.com>
2019-09-18 19:16:33 -0300

    Primeiras alterações

--- app/src/main/java/matc89/exercicio3/MainActivity.java
+++ app/src/main/java/matc89/exercicio3/MainActivity.java
@@ -2,12 +2,30 @@ package matc89.exercicio3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.style.TextAppearanceSpan;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttonAdicionar;
+    Button buttonRemover;
+    EditText editDescricao;
+    EditText editPrioridade;
+    TextView textPrioridade;
+    TextView textDescricao;
+    TextView textNovaTarefa;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonAdicionar = (Button)findViewById(R.id.buttonAdicionar);
+        buttonRemover = (Button)findViewById(R.id.buttonRemover);
+        editPrioridade = (EditText)findViewById(R.id.editPrioridade);
+        editDescricao = (EditText)findViewById(R.id.editDescricao);
+        
     }
 }
